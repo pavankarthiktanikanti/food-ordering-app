ecb40f7ebb3f5297bdffd6fd429aaa644599d275
Khilan10 <mahajankhilan1@gmail.com>
2020-03-07 22:19:05 +0530

    Add Code to show rating and price on a card for a restaurant #4

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import Header from '../../common/Header';
 import GridList from '@material-ui/core/GridList';
+import Grid from '@material-ui/core/Grid';
 import GridListTile from '@material-ui/core/GridListTile';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core';
+import { faRupeeSign, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /**
  * Custom Styles used to customize material ui components
  * @param {*} theme
  */
 const styles = theme => ({
+    /** set the styles for  Grid*/
+    root: {
+        flexGrow: 1,
+    },
     /** set the style for card text */
     cardText: {
         padding: '7%'
@@ -20,13 +27,28 @@ const styles = theme => ({
     /** set the style for ctaegories dispalyed on a card */
     categories: {
         fontSize: 'large',
-        marginTop: '1%'
+        marginTop: '16%',
+        marginBottom: '10%'
     },
     /** set style for the image displayed on the card */
     image: {
         height: '200px',
-        width: '500px'
+        width: '100%'
+    },
+    /** set style for rating box dispalyed in the crad */
+    ratingBox: {
+        backgroundColor: '#ffd966',
+        color: 'white',
+        padding: '2%',
+        fontSize: 'x-small',
+        fontWeight: 'bold'
+    },
+    /** set style for rupees displayed in card*/
+    rupees: {
+        float: 'right',
+        fontSize: 'small',
     }
+
 })
 
 class Home extends Component {
@@ -91,24 +113,39 @@ class Home extends Component {
                 <div className="home-container">
                     <GridList cellHeight={"auto"} cols={4}>
                         {rdata !== [] && rdata !== null && rdata.map(restaurant => (
-                            <GridListTile key={restaurant.id}>
-                                <Card variant="outlined">
-                                    <CardActionArea>
-                                        <CardContent>
-                                            <div>
-                                                <img src={restaurant.photo_URL} className={classes.image} alt={restaurant.id} />
-                                            </div>
-                                            <div className={classes.cardText}>
-                                                <Typography gutterBottom variant="h4" component="h2">
-                                                    {restaurant.restaurant_name}
-                                                </Typography>
-                                                <Typography variant="body2" component="p" className={classes.categories}>
-                                                    {restaurant.categories}
-                                                </Typography>
-                                            </div>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
+                            <GridListTile key={restaurant.id} >
+                                <Grid container className={classes.root} >
+                                    <Card >
+                                        <CardActionArea>
+                                            <CardContent>
+                                                <div>
+                                                    <img src={restaurant.photo_URL} className={classes.image} alt={restaurant.id} />
+                                                </div>
+                                                <div className={classes.cardText}>
+                                                    <Typography gutterBottom variant="h4" component="h2">
+                                                        {restaurant.restaurant_name}
+                                                    </Typography>
+                                                    <Typography variant="body2" component="p" className={classes.categories}>
+                                                        {restaurant.categories}
+                                                    </Typography>
+                                                    <div>
+                                                        <span className={classes.ratingBox}>
+                                                            <FontAwesomeIcon icon={faStar} /> &nbsp;
+                                                    <span> {restaurant.customer_rating}</span>
+                                                            &nbsp;
+                                                    ({restaurant.number_customers_rated})
+                                                </span>
+                                                        <span className={classes.rupees}>
+                                                            <FontAwesomeIcon icon={faRupeeSign} />
+                                                            {restaurant.average_price}
+                                                            &nbsp; for two
+                                                </span>
+                                                    </div>
+                                                </div >
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </Card>
+                                </Grid>
                             </GridListTile>
                         ))}
                     </GridList>
