43420c54620593d319fe24c39f3fe7fb4789cebb
pavankarthiktanikanti <pavankarthiktanikanti@gmail.com>
2020-03-08 12:32:43 +0530

    Show the list of items as a menu for Restaurant Detail Page #19
    - Add the items category wise and show the price of each item
    
    - Add responsive css to support various sized screens

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,8 +1,12 @@
-import { faRupeeSign, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faCircle, faRupeeSign, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { withStyles } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
 import React, { Component } from 'react';
+import 'typeface-roboto';
 import Header from '../../common/Header';
 import './Details.css';
 
@@ -18,6 +22,18 @@ const styles = theme => ({
     /* set font to bold */
     bold: {
         'font-weight': 600
+    },
+    /* Set the margin for category name */
+    category: {
+        marginBottom: '1%'
+    },
+    /* Set the margin for menu item */
+    menuItem: {
+        marginLeft: '2%'
+    },
+    /* Set the margin for the add icon (plus symbol) */
+    addIcon: {
+        marginLeft: '4%'
     }
 });
 
@@ -87,49 +103,92 @@ class Details extends Component {
                  * i.e. if uuid is not valid, then no data received from server and nothing will be displayed
                  */}
                 {this.state.restaurantDetails.uuid &&
-                    <div className='restaurant-info-container'>
-                        <div className='image-section'>
+                    <div>
+                        <div className='restaurant-info-container'>
+                            <div className='image-section'>
+                                {/**
+                                 * Show the restaurant image
+                                 */}
+                                <img className='restaurant-img' src={this.state.restaurantDetails.photoURL} alt={this.state.restaurantDetails.restaurantName} />
+                            </div>
                             {/**
-                             * Show the restaurant image
+                             * Show the details of the restaurant
                              */}
-                            <img className='restaurant-img' src={this.state.restaurantDetails.photoURL} alt={this.state.restaurantDetails.restaurantName} />
-                        </div>
-                        {/**
-                         * Show the details of the restaurant
-                         */}
-                        <div className='details-section'>
-                            <Typography variant='h4' component='h4' className={classes.detail}>
-                                {this.state.restaurantDetails.restaurantName}
-                            </Typography>
-                            <Typography variant='subtitle2' component='p' className={classes.detail}>
-                                {this.state.restaurantDetails.locality}
-                            </Typography>
-                            <Typography variant='subtitle2' component='p' className={classes.detail}>
-                                {this.state.restaurantDetails.categoriesText}
-                            </Typography>
-                            {/**
-                             * Show the ratings, no of customers rated along with the average price for two
-                             */}
-                            <div className='rating-price-container'>
-                                <div className='rating-section'>
-                                    <Typography variant='subtitle1' component='p' className={classes.bold}>
-                                        <FontAwesomeIcon icon={faStar} className='fa-star-icon' /> {this.state.restaurantDetails.customerRating}
+                            <div className='details-section'>
+                                <Typography variant='h4' component='h4' className={classes.detail}>
+                                    {this.state.restaurantDetails.restaurantName}
+                                </Typography>
+                                <Typography variant='subtitle2' component='p' className={classes.detail}>
+                                    {this.state.restaurantDetails.locality}
+                                </Typography>
+                                <Typography variant='subtitle2' component='p' className={classes.detail}>
+                                    {this.state.restaurantDetails.categoriesText}
+                                </Typography>
+                                {/**
+                                 * Show the ratings, no of customers rated along with the average price for two
+                                 */}
+                                <div className='rating-price-container'>
+                                    <div className='rating-section'>
+                                        <Typography variant='subtitle1' component='p' className={classes.bold}>
+                                            <FontAwesomeIcon icon={faStar} className='fa-star-icon' /> {this.state.restaurantDetails.customerRating}
+                                        </Typography>
+                                        <Typography variant='caption' component='p' className='caption-text'>
+                                            AVERAGE RATING BY <span className='no-of-customers'>{this.state.restaurantDetails.noOfCustomersRated}</span> CUSTOMERS
                                     </Typography>
-                                    <Typography variant='caption' component='p' className='caption-text'>
-                                        AVERAGE RATING BY <span className='no-of-customers'>{this.state.restaurantDetails.noOfCustomersRated}</span> CUSTOMERS
-                            </Typography>
-                                </div>
-                                <div className='price-section'>
-                                    <Typography variant='subtitle1' component='p' className={classes.bold}>
-                                        <FontAwesomeIcon icon={faRupeeSign} /> {this.state.restaurantDetails.averagePrice}
+                                    </div>
+                                    <div className='price-section'>
+                                        <Typography variant='subtitle1' component='p' className={classes.bold}>
+                                            <FontAwesomeIcon icon={faRupeeSign} /> {this.state.restaurantDetails.averagePrice}
+                                        </Typography>
+                                        <Typography variant='caption' component='p' className='caption-text'>
+                                            AVERAGE COST FOR TWO PEOPLE
                                     </Typography>
-                                    <Typography variant='caption' component='p' className='caption-text'>
-                                        AVERAGE COST FOR TWO PEOPLE
-                            </Typography>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                        <div id='items-checkout-container'>
+                            {/**
+                             * The list of menu items available in the restaurant along with their price
+                             */}
+                            <div className='items-container'>
+                                {this.state.restaurantDetails.categories.map(category => (
+                                    <div key={category.id}>
+                                        {/**
+                                         * Show the category name
+                                         */}
+                                        <Typography className={classes.category}>
+                                            <span className='category-name'>{category.category_name}</span>
+                                        </Typography>
+                                        <Divider />
+                                        {category.item_list.map(item => (
+                                            <div className='menu-item-section' key={item.id}>
+                                                {/**
+                                                 * Show the circle based on item time red(non veg)/green(veg)
+                                                 */}
+                                                {'VEG' === item.item_type && <FontAwesomeIcon icon={faCircle} className='fa-circle-green' />}
+                                                {'NON_VEG' === item.item_type && <FontAwesomeIcon icon={faCircle} className='fa-circle-red' />}
+
+                                                <Typography className={classes.menuItem}>
+                                                    <span className='menu-item'>{item.item_name}</span>
+                                                </Typography>
+                                                {/**
+                                                 * Show rupee symbol and the price of the item with a plus sign icon to add to cart
+                                                 */}
+                                                <span className='item-price'>
+                                                    <FontAwesomeIcon icon={faRupeeSign} className='fa-rupee' />{item.price}
+                                                </span>
+                                                <IconButton className={classes.addIcon}><AddIcon /></IconButton>
+                                            </div>
+                                        ))}
+                                    </div>
+                                ))}
+                            </div>
+                            <div className='cart-checkout-container'>
+
+                            </div>
+                        </div>
+                    </div >
                 }
             </div>
         )
