b22ff7a06c88c479f6c72135d35c7fb60dc8f529
Khilan10 <mahajankhilan1@gmail.com>
2020-03-13 19:35:06 +0530

    Add functionality to restrict a logged in customer to directly go to Checkout Page without going through Details Page #22

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import Header from '../../common/Header';
 
 class Checkout extends Component {
+
+    /** this method will be called before the components get mount */
+    UNSAFE_componentWillMount() {
+        /** 
+         *  If the logged in person go's to URL of the checkout page in the 
+         *  browser’s address bar without going through the details page than
+         *  customer will be redirected to home page
+         */
+        if (this.props.location.state === undefined) {
+            this.props.history.push({
+                pathname: '/'
+            })
+        }
+    }
+
     render() {
         return (
             <div>
